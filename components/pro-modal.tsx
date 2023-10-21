6f71bbe47b7b4268e8a3eb250475f010f16e97de
Broque Thomas <batmanesignal@gmail.com>
2023-10-21 00:16:14 -0700

    Update pro-modal.tsx

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -5,13 +5,17 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } f
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const ProModal = () => {
     const proModal = useProModal();
     const {toast} = useToast();
     const [loading, setLoading] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
     const onSubscribe = async () => {
             try{
                 setLoading(true);
@@ -26,6 +30,7 @@ const ProModal = () => {
                 setLoading(false);
             }
     }
+    if(!isMounted) return null;
     return ( 
         <Dialog
         open={proModal.isOpen}
